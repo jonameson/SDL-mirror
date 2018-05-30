c31fc6be10fc661dbc1b05306ff4ba5fac5d29f0
Jona <jona@visualblasters.com>
2018-05-30 15:57:36 -0400

    [Android] Fixed mk build script path issues

--- AndroidAudioOnly.mk
+++ AndroidAudioOnly.mk
@@ -10,40 +10,39 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := SDL2
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/sdl/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,, \
-	$(LOCAL_PATH)/sdl/src/atomic/SDL_atomic.c \
-	$(LOCAL_PATH)/sdl/src/atomic/SDL_spinlock.c.arm \
-	$(LOCAL_PATH)/sdl/src/core/android/SDL_android_audio_only.c \
-	$(wildcard $(LOCAL_PATH)/sdl/src/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/audio/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/audio/android/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/audio/dummy/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/cpuinfo/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/dynapi/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/events/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/file/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/haptic/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/haptic/dummy/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/joystick/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/joystick/dummy/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/loadso/dummy/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/power/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/filesystem/dummy/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/render/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/render/*/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/stdlib/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/thread/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/thread/pthread/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/timer/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/timer/unix/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/video/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/video/yuv2rgb/*.c) \
-	$(wildcard $(LOCAL_PATH)/sdl/src/video/dummy/*.c))
+	$(wildcard $(LOCAL_PATH)/src/core/android/SDL_android_audio_only.c) \
+	$(wildcard $(LOCAL_PATH)/src/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/audio/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/audio/android/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/audio/dummy/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/atomic/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/cpuinfo/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/dynapi/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/events/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/file/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/haptic/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/haptic/dummy/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/joystick/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/joystick/dummy/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/loadso/dummy/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/power/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/filesystem/dummy/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/render/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/render/*/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/stdlib/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/thread/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/thread/pthread/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/timer/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/timer/unix/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/video/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/video/yuv2rgb/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/video/dummy/*.c))
 
 # We have added a custom flag in the SDL config for android
 # to disable as much stuff as possible so only audio is used.
